5a9333b9e1ea2894d02fc17f7ef7cc65170a8ccb
Groverkss <groverkss@gmail.com>
2021-05-01 01:30:12 +0530

    Basic RPC calling between worker and master working

---
+++ mr/coordinator.go
@@ -0,0 +1,167 @@
+package mr
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
+
+type mapTask struct {
+	done     bool
+	worker   int
+	filename string
+}
+
+type reduceTask struct {
+	done   bool
+	worker int
+}
+
+type Coordinator struct {
+	mapTasks    []mapTask
+	reduceTasks []reduceTask
+
+	availableMapTasks    map[int]int
+	availableReduceTasks map[int]int
+
+	mapDone    bool
+	reduceDone bool
+}
+
+// Your code here -- RPC handlers for the worker to call.
+
+//
+// an example RPC handler.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+func (c *Coordinator) GetTask(args *GetArgs, reply *GetReply) error {
+	fmt.Printf("Worker %v requesting for task\n", args.WorkerId)
+	// If map task available, send map task
+	for id, _ := range c.availableMapTasks {
+		// Populate reply
+		reply.TaskType = 0
+		reply.TaskNum = id
+		reply.Filename = c.mapTasks[id].filename
+
+		// Fill in maptask details
+		c.mapTasks[id].worker = args.WorkerId
+
+		// Remove from available
+		delete(c.availableMapTasks, id)
+
+		// Run waiting thread
+		// go waitFor10Sec()
+
+		fmt.Printf("Map Task %v given to worker %v\n", id, args.WorkerId)
+
+		return nil
+	}
+
+	// Not task available right now
+	if !c.mapDone {
+		fmt.Printf("No tasks available for worker %v\n", args.WorkerId)
+		reply.TaskType = 2
+		return nil
+	}
+
+	// TODO
+	// If all map tasks over and reduce task available send reduce task
+	reply.TaskType = 2
+	return nil
+
+	// Not task available right now
+}
+
+func (c *Coordinator) FinishTask(args *FinishArgs, reply *FinishReply) error {
+	// TODO: Add locks
+
+	if args.TaskType == 0 {
+		// Map task finished
+		if c.mapTasks[args.TaskNum].worker == args.WorkerId {
+			fmt.Printf("Worker %v finished map task %v\n",
+				args.WorkerId, args.TaskNum)
+			c.mapTasks[args.TaskNum].done = true
+			c.mapTasks[args.TaskNum].worker = -1
+		} else {
+			fmt.Printf("Worker %v finished map task %v too late\n",
+				args.WorkerId, args.TaskNum)
+		}
+		return nil
+	} else {
+		// Reduce task finished
+		if c.reduceTasks[args.TaskNum].worker == args.WorkerId {
+			fmt.Printf("Worker %v finished reduce task %v\n",
+				args.WorkerId, args.TaskNum)
+			c.reduceTasks[args.TaskNum].done = true
+			c.reduceTasks[args.TaskNum].worker = -1
+		} else {
+			fmt.Printf("Worker %v finished reduce task %v too late\n",
+				args.WorkerId, args.TaskNum)
+		}
+		return nil
+	}
+}
+
+//
+// start a thread that listens for RPCs from worker.go
+//
+func (c *Coordinator) server() {
+	rpc.Register(c)
+	rpc.HandleHTTP()
+	//l, e := net.Listen("tcp", ":1234")
+	sockname := coordinatorSock()
+	os.Remove(sockname)
+	l, e := net.Listen("unix", sockname)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
+
+//
+// main/mrcoordinator.go calls Done() periodically to find out
+// if the entire job has finished.
+//
+func (c *Coordinator) Done() bool {
+	ret := false
+
+	// Your code here.
+
+	return ret
+}
+
+//
+// create a Coordinator.
+// main/mrcoordinator.go calls this function.
+// nReduce is the number of reduce tasks to use.
+//
+func MakeCoordinator(files []string, nReduce int) *Coordinator {
+	c := Coordinator{}
+
+	c.mapDone, c.reduceDone = false, false
+
+	// Fill map tasks
+	c.mapTasks = make([]mapTask, len(files))
+	c.availableMapTasks = make(map[int]int)
+
+	for i, _ := range c.mapTasks {
+		c.mapTasks[i] = mapTask{false, -1, files[i]}
+		c.availableMapTasks[i] = i
+	}
+
+	// Fill reduce tasks
+	c.reduceTasks = make([]reduceTask, nReduce)
+	c.availableReduceTasks = make(map[int]int)
+
+	for i, _ := range c.reduceTasks {
+		c.reduceTasks[i] = reduceTask{false, -1}
+		c.availableReduceTasks[i] = i
+	}
+
+	c.server()
+	return &c
+}
---
+++ mr/worker.go
@@ -0,0 +1,108 @@
+package mr
+
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"time"
+)
+
+//
+// Map functions return a slice of KeyValue.
+//
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+//
+// use ihash(key) % NReduce to choose the reduce
+// task number for each KeyValue emitted by Map.
+//
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+func mapFunc(mapf func(string, string) []KeyValue,
+	taskType int,
+	filename string) {
+}
+
+//
+// main/mrworker.go calls this function.
+//
+func Worker(mapf func(string, string) []KeyValue,
+	reducef func(string, []string) string) {
+
+	workerId := 0
+
+	for {
+		// Ask master for task
+		switch reply := callGetTask(workerId); reply.TaskType {
+		case 0:
+			fmt.Printf("[Worker %v]: Recieved map task from master\n", workerId)
+			mapFunc(mapf, reply.TaskNum, reply.Filename)
+			callFinishTask(reply.TaskType, workerId, reply.TaskNum)
+			// Map
+		case 1:
+			fmt.Printf("[Worker %v]: Recieved reduce task from master\n",
+				workerId)
+			// Reduce
+		default:
+			// Sleep for a minute
+			fmt.Printf("[Worker %v]: No available task from master\n", workerId)
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+//
+// example function to show how to make an RPC call to the coordinator.
+//
+// the RPC argument and reply types are defined in rpc.go.
+//
+
+func callGetTask(workerId int) *GetReply {
+	args, reply := GetArgs{workerId}, GetReply{}
+
+	ok := call("Coordinator.GetTask", &args, &reply)
+
+	if !ok {
+		fmt.Printf("Could not reach master\n")
+		reply.TaskType = 99
+		return &reply
+	}
+
+	return &reply
+}
+
+func callFinishTask(taskType, workerId, taskId int) {
+	args, reply := FinishArgs{taskType, workerId, taskId}, FinishReply{}
+	call("Coordinator.FinishTask", &args, &reply)
+}
+
+//
+// send an RPC request to the coordinator, wait for the response.
+// usually returns true.
+// returns false if something goes wrong.
+//
+func call(rpcname string, args interface{}, reply interface{}) bool {
+	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	sockname := coordinatorSock()
+	c, err := rpc.DialHTTP("unix", sockname)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	defer c.Close()
+
+	err = c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
+}
